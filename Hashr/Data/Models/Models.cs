80469b34e87a7e33eeac99e84a3a8015c5f65524
Daniel K <daniel.k@bytestxck.software>
2023-05-23 13:12:30 +0200

    Projektdateien hinzufügen.

---
+++ Hashr/Data/Models/Models.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hashr.Data.Models
+{
+    public enum HashAlgorithm
+    {
+        MD5,
+        SHA1,
+        SHA256,
+        SHA384,
+        SHA512,
+        SHA3256,
+        SHA3512,
+        RIPEMD128,
+        RIPEMD160,
+        Whirlpool
+    }
+}
---
+++ Hashr/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Hashr
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
